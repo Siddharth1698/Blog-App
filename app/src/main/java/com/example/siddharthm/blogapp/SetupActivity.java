d3678af2607935e7448c9f1cf853adf5a8e72633
Siddharth M <siddharth.m98@gmail.com>
2018-07-02 16:17:54 +0530

    Adding Cropping feature User Setup

--- app/src/main/java/com/example/siddharthm/blogapp/SetupActivity.java
+++ app/src/main/java/com/example/siddharthm/blogapp/SetupActivity.java
@@ -1,13 +1,53 @@
 package com.example.siddharthm.blogapp;
 
+import android.content.Intent;
+import android.net.Uri;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ImageButton;
+
+import com.theartofdev.edmodo.cropper.CropImage;
+import com.theartofdev.edmodo.cropper.CropImageView;
 
 public class SetupActivity extends AppCompatActivity {
+    private EditText setupName;
+    private ImageButton setupImageBtn;
+    private Button setupSubmitBtn;
+    private static final int GALLERY_REQUEST = 1;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_setup);
+        setupName = (EditText)findViewById(R.id.setupImageFeild);
+        setupImageBtn = (ImageButton)findViewById(R.id.setupImageButton);
+        setupSubmitBtn = (Button)findViewById(R.id.setUpSubmitButton);
+
+        setupSubmitBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent galleryIntent = new Intent();
+                galleryIntent.setAction(Intent.ACTION_GET_CONTENT);
+                galleryIntent.setType("image/*");
+                startActivityForResult(galleryIntent,GALLERY_REQUEST);
+
+            }
+        });
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+
+        if (requestCode == GALLERY_REQUEST && resultCode == RESULT_OK){
+            Uri imageUri = data.getData();
+            CropImage.activity()
+                    .setGuidelines(CropImageView.Guidelines.ON)
+                    .start(this);
+
+        }
     }
 }
