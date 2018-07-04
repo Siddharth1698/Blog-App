4914c89c6d1ff377b7621a794cab13846d5837bb
Siddharth M <siddharth.m98@gmail.com>
2018-07-04 22:45:34 +0530

    Adding Offline Feature

--- app/src/main/java/com/example/siddharthm/blogapp/BlogApp.java
+++ app/src/main/java/com/example/siddharthm/blogapp/BlogApp.java
@@ -2,9 +2,13 @@ package com.example.siddharthm.blogapp;
 
 import android.app.Application;
 
+import com.google.firebase.database.FirebaseDatabase;
+
 public class BlogApp extends Application {
     @Override
     public void onCreate() {
         super.onCreate();
+
+        FirebaseDatabase.getInstance().setPersistenceEnabled(true);
     }
 }
